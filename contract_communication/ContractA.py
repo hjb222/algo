9b89969ed2a046b6e7eff8ab7004b31d0b0a0ce6
Harry <hjb222@lehigh.edu>
2023-02-27 18:14:50 -0500

    added basic functionality

--- contract_communication/ContractA.py
+++ contract_communication/ContractA.py
@@ -8,9 +8,34 @@ APP_CREATOR = Seq(creator := AppParam.creator(Int(0)), creator.value())
  
 class Demo(Application):
  
+    global_counter: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, default=Int(0))
+    winning_call_number: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, default=Int(5))
+
     @create
     def create(self):
         return self.initialize_application_state()
+    
+    @external
+    def getGlobalCounter(self,*,output:abi.Uint64):
+        return output.set(self.global_counter.get())
+    
+    @internal 
+    def winner(self, acct: abi.Address):
+            return self.global_counter.set(self.global_counter.get()+Int(5))
+    
+    @internal
+    def increaseCounter(self, acct: abi.Address):
+        return If(self.global_counter.get()==self.winning_call_number.get(),
+                  self.winner(acct),
+                  self.global_counter.set(self.global_counter.get()+Int(1)))
+    
+    @external
+    def add(self, num1: abi.Uint64, num2: abi.Uint64, *, output: abi.Uint64):
+        return Seq(
+             self.increaseCounter(Txn.sender()),
+             output.set(num1.get()+num2.get())
+        )
+            
 
 if __name__ == "__main__":
     app = Demo(version=8)
